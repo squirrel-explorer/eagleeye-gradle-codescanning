bbc0ea4eeab0484e3f4044ce116789c02ef8cf19
squirrel-explorer <xvider.zx@gmail.com>
2016-07-23 20:23:19 +0800

    增加Gradle兼容性支持，最低支持到Gradle 2.2和Gradle插件1.1.0

--- plugin/src/main/groovy/com/squirrel_explorer/eagleeye/codescanning/BaseLintTask.groovy
+++ plugin/src/main/groovy/com/squirrel_explorer/eagleeye/codescanning/BaseLintTask.groovy
@@ -1,6 +1,5 @@
 package com.squirrel_explorer.eagleeye.codescanning
 
-import com.android.build.gradle.internal.LintGradleClient
 import com.android.builder.model.AndroidProject
 import com.android.builder.model.Variant
 import com.android.tools.lint.*
@@ -11,6 +10,7 @@ import org.gradle.api.GradleException
 import org.gradle.tooling.GradleConnector
 import org.gradle.tooling.ProjectConnection
 
+import java.lang.reflect.Constructor
 import java.lang.reflect.Field
 
 /**
@@ -34,43 +34,162 @@ public abstract class BaseLintTask extends DefaultTask {
         if (project.plugins.hasPlugin('java')) {
             client = new LintCliClient(flags, 'java-cli')
         } else {
-            GradleConnector gradleConn = GradleConnector.newConnector()
-            gradleConn.forProjectDirectory(project.projectDir)
-            ProjectConnection prjConn = gradleConn.connect()
-            AndroidProject androidProject = prjConn.getModel(AndroidProject.class)
-            if (null == androidProject) {
-                throw new GradleException('No valid Android project.')
+            client = createLintGradleClient(productFlavor, buildType)
+            if (null == client) {
+                throw new GradleException('No proper constructor of \'com.android.build.gradle.internal.LintGradleClient\' is declared')
             }
-
-            Collection<Variant> variantList = androidProject.getVariants()
-            if (null == variantList || variantList.isEmpty()) {
-                throw new GradleException('No variant defined.')
-            }
-            String targetVariantName = new StringBuilder().append(productFlavor).append(buildType).toString()
-            Variant variant = null
-            for (int i = 0; i < variantList.size(); i++) {
-                if (variantList.getAt(i).getDisplayName().equalsIgnoreCase(targetVariantName)) {
-                    variant = variantList.getAt(i)
-                    break
-                }
-            }
-            if (null == variant) {
-                variant = variantList.getAt(0)
-            }
-
-            client = new LintGradleClient(
-                    registry,
-                    flags,
-                    project,
-                    androidProject,
-                    null,
-                    variant,
-                    null
-            )
         }
     }
 
+    // 2.0.0 - latest
+    private static final String SIGNATURE_2_0_0 = '[class com.android.tools.lint.client.api.IssueRegistry, class com.android.tools.lint.LintCliFlags, interface org.gradle.api.Project, interface com.android.builder.model.AndroidProject, class java.io.File, interface com.android.builder.model.Variant, class com.android.sdklib.BuildToolInfo]'
+    // 1.5.0
+    private static final String SIGNATURE_1_5_0 = '[class com.android.tools.lint.client.api.IssueRegistry, class com.android.tools.lint.LintCliFlags, interface org.gradle.api.Project, interface com.android.builder.model.AndroidProject, class java.io.File, class java.lang.String, class com.android.sdklib.BuildToolInfo]'
+    // 1.1.0 - 1.3.0
+    private static final String SIGNATURE_1_1_0 = '[class com.android.tools.lint.client.api.IssueRegistry, class com.android.tools.lint.LintCliFlags, interface org.gradle.api.Project, interface com.android.builder.model.AndroidProject, class java.io.File, class java.lang.String]'
+
+    private LintCliClient createLintGradleClient(String productFlavor, String buildType) {
+        GradleConnector gradleConn = GradleConnector.newConnector()
+        gradleConn.forProjectDirectory(project.projectDir)
+        ProjectConnection prjConn = gradleConn.connect()
+        AndroidProject androidProject = prjConn.getModel(AndroidProject.class)
+        if (null == androidProject) {
+            throw new GradleException('No valid Android project.')
+        }
+
+        Collection<Variant> variantList = androidProject.getVariants()
+        if (null == variantList || variantList.isEmpty()) {
+            throw new GradleException('No variant defined.')
+        }
+        String targetVariantName = new StringBuilder().append(productFlavor).append(buildType).toString()
+        Variant variant = null
+        for (int i = 0; i < variantList.size(); i++) {
+            if (variantList.getAt(i).getName().equalsIgnoreCase(targetVariantName)) {
+                variant = variantList.getAt(i)
+                break
+            }
+        }
+        if (null == variant) {
+            variant = variantList.getAt(0)
+        }
+
+        LintCliClient lintClient = null
+
+        try {
+            Class clazz = Class.forName('com.android.build.gradle.internal.LintGradleClient')
+            if (null != clazz) {
+                Constructor[] constructors = clazz.getConstructors()
+                if (null != constructors && constructors.length > 0) {
+                    for (Constructor constructor : constructors) {
+                        String signature = constructor.getParameterTypes().toString()
+                        if (SIGNATURE_2_0_0.equals(signature)) {
+                            lintClient = (LintCliClient)constructor.newInstance(
+                                    registry,
+                                    flags,
+                                    project,
+                                    androidProject,
+                                    null,
+                                    variant,
+                                    null)
+                            break
+                        } else if (SIGNATURE_1_5_0.equals(signature)) {
+                            lintClient = (LintCliClient)constructor.newInstance(
+                                    registry,
+                                    flags,
+                                    project,
+                                    androidProject,
+                                    null,
+                                    variant.getName(),
+                                    null)
+                            break
+                        } else if (SIGNATURE_1_1_0.equals(signature)) {
+                            lintClient = (LintCliClient)constructor.newInstance(
+                                    registry,
+                                    flags,
+                                    project,
+                                    androidProject,
+                                    null,
+                                    variant.getName())
+                            break
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace()
+            lintClient = null
+        }
+
+        return lintClient
+    }
+
+    /*
+    private int compareVersion(String version1, String version2) {
+        int[] digits1 = extraceVersions(version1)
+        int[] digits2 = extraceVersions(version2)
+
+        if (null == digits1 || 0 == digits1.length) {
+            if (null == digits2 || 0 == digits2.length) {
+                return 0
+            } else {
+                return -1
+            }
+        } else {
+            if (null == digits2 || 0 == digits2.length) {
+                return 1
+            } else {
+                int size = (digits1.length < digits2.length ? digits1.length : digits2.length)
+                int ret = 0
+                for (int i = 0; i < size; i++) {
+                    ret = digits1[i] - digits2[i]
+                    if (0 != ret) {
+                        break
+                    }
+                }
+                if (0 == ret) {
+                    if (digits1.length > size) {
+                        ret = 1
+                    } else {
+                        ret = -1
+                    }
+                }
+                return ret
+            }
+        }
+    }
+
+    private int[] extraceVersions(String version) {
+        if (null == version || version.isEmpty()) {
+            return null
+        }
+
+        String digitalVersion = version.split('-')[0].trim()
+        if (null == digitalVersion || digitalVersion.isEmpty()) {
+            return null
+        }
+
+        String[] digitalVersions = digitalVersion.split('\\.')
+        if (null == digitalVersions || 0 == digitalVersions.length) {
+            return null
+        }
+
+        int[] digits = new int[digitalVersions.length]
+        for (int i = 0; i < digitalVersions.length; i++) {
+            try {
+                digits[i] = Integer.parseInt(digitalVersions[i])
+            } catch (Exception e) {
+                digits[i] = 0
+            }
+        }
+        return digits
+    }
+    */
+
     protected void addReporters(File textOutput, File htmlOutput, File xmlOutput) {
+        if (null == client) {
+            return
+        }
+
         List<Reporter> reporters = flags.getReporters()
         Reporter reporter
         boolean needDefaultReporter = true
